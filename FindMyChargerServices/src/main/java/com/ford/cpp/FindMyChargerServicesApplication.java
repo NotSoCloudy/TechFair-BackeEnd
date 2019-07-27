75513ab4ad4cb0e43861a93b44091fec89636a88
V.Sharma <vishal.sharma@perficient.com>
2019-07-27 01:48:27 -0400

    Empty Backend Project

---
+++ FindMyChargerServices/src/main/java/com/ford/cpp/FindMyChargerServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ford.cpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyChargerServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMyChargerServicesApplication.class, args);
+	}
+
+}
---
+++ FindMyChargerServices/src/test/java/com/ford/cpp/FindMyChargerServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ford.cpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyChargerServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
